bca584802f042bd8b62eec3b43da819679011aaf
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-03-25 23:45:40 +0100

    fixed

--- CesiZen-Client/src/app/app.routes.ts
+++ CesiZen-Client/src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-  {
-    path: '', component: AppComponent
-    //loadComponent: () => import('./home.component').then(m => m.HomeComponent)
-  },
-  // Other routes...
-];
+export const routes: Routes = [];
--- CesiZen-Client/src/test.ts
+++
@@ -1,6 +0,0 @@
-import 'zone.js/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { getTestBed } from '@angular/core/testing';
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
