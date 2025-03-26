afa694bd5d205cc50b4b77bee521742521e59501
YaeBo <YaeBo@DELLYASSISTANT>
2025-03-26 19:12:04 +0100

    feat(view): generated exercises component

---
+++ CesiZen-Client/src/app/exercises/exercises.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercises',
+  standalone: true,
+  templateUrl: './exercises.component.html',
+  styleUrl: './exercises.component.css'
+})
+export class ExercisesComponent {
+
+}
