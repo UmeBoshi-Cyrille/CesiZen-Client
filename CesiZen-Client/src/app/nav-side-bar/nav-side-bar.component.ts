e9b14838b584ae905b050e6e9503fa927d0a2fe6
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-03 16:21:21 +0200

    feature(view);add active link effect

--- CesiZen-Client/src/app/nav-side-bar/nav-side-bar.component.ts
+++ CesiZen-Client/src/app/nav-side-bar/nav-side-bar.component.ts
@@ -1,12 +1,12 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav-side-bar',
   standalone: true,
-  imports: [NgStyle, RouterLink, RouterModule],
+  imports: [NgStyle, RouterLink, RouterModule, RouterLinkActive],
   templateUrl: './nav-side-bar.component.html',
   styleUrl: './nav-side-bar.component.scss'
 })
