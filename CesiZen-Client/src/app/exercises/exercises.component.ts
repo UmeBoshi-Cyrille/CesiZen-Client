3870dd1faf3143c006ed324a15a9822be299b69d
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-08 21:57:42 +0200

    feature(view); add exercises page

--- CesiZen-Client/src/app/exercises/exercises.component.ts
+++ CesiZen-Client/src/app/exercises/exercises.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ExerciseQueryService } from '../../services/exercise/exercise-query.service';
+import { Exercise } from '../../models/exercise/exercise';
 
 @Component({
   selector: 'app-exercises',
   standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './exercises.component.html',
-  styleUrl: './exercises.component.css'
+  styleUrl: './exercises.component.scss'
 })
-export class ExercisesComponent {
+export class ExercisesComponent implements OnInit{
+  exercises: Exercise[] = [];
+  constructor(
+    private exerciseQueryService: ExerciseQueryService,
+  ) {}
+  ngOnInit() {
+    this.exerciseQueryService.getAllExercises(58).subscribe(
+      exercises => this.exercises = exercises
+    );
+    }
 
 }
