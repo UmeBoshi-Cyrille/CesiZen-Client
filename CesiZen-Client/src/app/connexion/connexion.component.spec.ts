b83e640fe82ae3bb2857561a45b66134e953988e
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-15 23:25:24 +0200

    fix-test): connexion http provider missing

--- CesiZen-Client/src/app/connexion/connexion.component.spec.ts
+++ CesiZen-Client/src/app/connexion/connexion.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { ConnexionComponent } from './connexion.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ConnexionQueryService } from '../../services/connexion/connexion-query.service';
 
 describe('SeConnecterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ConnexionComponent],
+      providers: [
+        provideHttpClient(), // Provide HttpClient
+        ConnexionQueryService, // Provide any services used by the component
+      ],
     })
       .compileComponents();
   });
