f31cf781e9f3b9d7b80e0504801bd7113670196e
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-15 22:56:30 +0200

    feature(view;)progressed toggle side-bar and router for login and logout

--- CesiZen-Client/src/app/nav-side-bar/nav-side-bar.component.ts
+++ CesiZen-Client/src/app/nav-side-bar/nav-side-bar.component.ts
@@ -1,21 +1,38 @@
-import { NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgIf, NgStyle } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav-side-bar',
   standalone: true,
-  imports: [NgStyle, RouterLink, RouterModule, RouterLinkActive],
+  imports: [NgStyle, NgIf, RouterLink, RouterModule, RouterLinkActive],
   templateUrl: './nav-side-bar.component.html',
   styleUrl: './nav-side-bar.component.scss'
 })
 export class NavSideBarComponent {
+  username: string | null = null;
+  isLoggedIn: boolean = false;
   constructor(
     private route: RouterModule
-  ) { }
+  ) {
+    this.username = localStorage.getItem('username');
+    this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+}
   status: boolean = false;
   clickEvent() {
     this.status = !this.status;
+  }
+  @HostListener('document:click', ['$event'])
+  closeNavOnOutsideClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (this.status && !target.closest('nav') && !target.closest('.openbtn')) {
+      this.status = false;
+    }
+  }
+  logout() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = '/se-connecter';
   }
 }
