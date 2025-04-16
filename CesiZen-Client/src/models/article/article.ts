b387d31a45777fd05c39193f5f9ed11418eb056f
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-16 23:40:17 +0200

    fixed(view):renamed folder and fixed errors

---
+++ CesiZen-Client/src/models/article/article.ts
@@ -0,0 +1,15 @@
+export class Article {
+  
+  constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public author: string,
+    public content: string,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date(),
+    public imagePath: string,
+    public categories = 0,
+  ) {
+  }
+}
