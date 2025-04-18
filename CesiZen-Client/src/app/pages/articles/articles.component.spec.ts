b1543d9b7cf0e605dd28d9808161dcd84f764097
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-04-18 10:06:48 +0200

    refacto(pages): moved article into page folder

--- CesiZen-Client/src/app/pages/articles/articles.component.spec.ts
+++ CesiZen-Client/src/app/pages/articles/articles.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { ArticlesComponent } from './articles.component';
 import { provideHttpClient } from '@angular/common/http';
-import { ArticleQueryService } from '../../services/article/article-query.service';
+import { ArticleQueryService } from '../../../services/article/article-query.service';
 
 
 describe('ArticlesComponent', () => {
