a960ca62f9d0a71f590e3d5512b2d8af7885c6b6
Cyrille <98464975+UmeBoshi-Cyrille@users.noreply.github.com>
2025-04-20 20:15:45 +0200

    401 error handler and toast (#16)
    * feat(service) toaster added for error interceptor
    
    * feat(service): add success interceptor to send toast.
    
    * fix(test): missing providers added

--- CesiZen-Client/src/app/app.component.ts
+++ CesiZen-Client/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '@components/footer/footer.component';
 import { NavSideBarComponent } from '@components/nav-side-bar/nav-side-bar.component';
+import { ToastNotifierService } from './services/toast/toast-notifier.service';
+import { Toast } from 'primeng/toast';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavSideBarComponent,FooterComponent, RouterOutlet],
+  imports: [NavSideBarComponent,FooterComponent, RouterOutlet, Toast],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'CesiZen-Client';
+
+  constructor(private toast: ToastNotifierService) { }
 }
--- CesiZen-Client/src/app/app.config.ts
+++ CesiZen-Client/src/app/app.config.ts
@@ -7,12 +7,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './common/interceptor/error.interceptor';
+import { successInterceptor } from './common/interceptor/success-interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideZoneChangeDetection({ eventCoalescing: true }),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, successInterceptor])),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: Aura } }),
   ],
 };
