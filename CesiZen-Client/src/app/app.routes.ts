372db402cc18783de6d4e1968316d8abd984170c
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-21 15:14:07 +0200

    fix(view): add the route for articles by category

--- CesiZen-Client/src/app/app.routes.ts
+++ CesiZen-Client/src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { ExercisesComponent } from '@pages/exercises/exercises.component';
 import { HomeComponent } from '@pages/home/home.component';
 import { RegistrationComponent } from '@pages/register/register.component';
 import { SingleArticleComponent } from '@pages/single-article/single-article.component';
+import { ByCategoryComponent } from '@pages/by-category/by-category.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'articles', component: ArticlesComponent },
   { path: 'articles/:id', component: SingleArticleComponent },
+  { path: 'articles/category/:id', component: ByCategoryComponent },
   { path: 'exercises', component: ExercisesComponent },
   { path: 'exercise-form', component: ExerciseFormComponent },
 
