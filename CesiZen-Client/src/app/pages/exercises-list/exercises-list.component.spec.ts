fbf956e385391d592611164ed0385fec9f42b3ae
YaeBo <YaeBo@DELLYASSISTANT>
2025-04-22 20:48:51 +0200

    fix(view): fixed test issue

--- CesiZen-Client/src/app/pages/exercises-list/exercises-list.component.spec.ts
+++ CesiZen-Client/src/app/pages/exercises-list/exercises-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExercisesListComponent } from './exercises-list.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ExercisesListComponent', () => {
   let component: ExercisesListComponent;
@@ -8,7 +9,10 @@ describe('ExercisesListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExercisesListComponent]
+      imports: [ExercisesListComponent],
+      providers: [
+        provideHttpClient(), // Provide HttpClient
+      ],
     })
     .compileComponents();
 
