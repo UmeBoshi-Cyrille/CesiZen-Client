9e19a7325ad4454911039f8bd42df75fc58cb68b
Umeboshi <umehara_cyrille@outlook.com>
2025-05-07 22:18:45 +0200

    feat(component): admin home page created

--- CesiZen-Client/src/app/back-pages/back-home/back-home.component.ts
+++ CesiZen-Client/src/app/back-pages/back-home/back-home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { BackArticlesComponent } from '../back-articles/back-articles.component';
+import { BackCategoryComponent } from '../back-category/back-category.component';
+import { BackUsersComponent } from '../back-users/back-users.component';
+import { UserDataStorage } from '@models/user/user-data-storage';
 
 @Component({
   selector: 'app-back-home',
-  imports: [],
+  standalone: true,
+  imports: [BackArticlesComponent, BackCategoryComponent, BackUsersComponent],
   templateUrl: './back-home.component.html',
   styleUrl: './back-home.component.scss'
 })
 export class BackHomeComponent {
+  userData: UserDataStorage | null = null;
 
+  constructor() {
+    const storedAccount = localStorage.getItem('userData');
+
+    if (storedAccount) {
+      this.userData = JSON.parse(storedAccount);
+    }
+  }
 }
