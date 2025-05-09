cb45f458237fb4ccdc73de3fc6c8d6f23e2b19c2
YaeBo <YaeBo@DELLYASSISTANT>
2025-05-10 00:08:13 +0200

    fixed(view;)all pages for mobile

--- CesiZen-Client/src/app/pages/email-verification/email-verification.component.ts
+++ CesiZen-Client/src/app/pages/email-verification/email-verification.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-email-verification',
+  standalone: true,
   imports: [],
   templateUrl: './email-verification.component.html',
   styleUrl: './email-verification.component.scss'
