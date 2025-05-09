b548ed5082b4324845d88f36b66e466c68606068
YaeBo <YaeBo@DELLYASSISTANT>
2025-05-09 17:05:05 +0200

    feature(view;)add nav-mobile fonction and style

--- CesiZen-Client/src/app/app.component.ts
+++ CesiZen-Client/src/app/app.component.ts
@@ -2,16 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '@components/footer/footer.component';
 import { NavSideBarComponent } from '@components/nav-side-bar/nav-side-bar.component';
+import { NavMobileComponent } from './components/nav-mobile/nav-mobile.component';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavSideBarComponent, FooterComponent, RouterOutlet],
+  imports: [NavSideBarComponent, FooterComponent, RouterOutlet, NavMobileComponent, CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'CesiZen-Client';
+  isMobileScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(['(max-width: 700px)']).subscribe(result => {
+      this.isMobileScreen = result.matches;
+    });
+  }
 
 }
